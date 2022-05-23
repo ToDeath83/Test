a01c4aebc84d8de5d45ec6b0b4595e7694fbb784
Sergey Katalymov <todeath83@mail.ru>
2022-05-23 10:04:08 +0300

    1.3

--- src/main.java
+++ src/main.java
@@ -6,5 +6,8 @@ public class main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("!!!!!!!!!!!!!");
+
     }
 }
